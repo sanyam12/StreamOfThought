af07ed841c7d0452d507c95d4d3853aec0460be5
Sanyam Ratreja <sanyamratreja18@gmail.com>
2022-01-19 14:43:09 +0530

    profile page design ready

---
+++ app/src/main/java/com/unravel/streamofthought/test.kt
@@ -0,0 +1,11 @@
+package com.unravel.streamofthought
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class test:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test)
+    }
+}
--- app/src/main/java/com/unravel/streamofthought/ui/slideshow/splashscreen.kt
+++ app/src/main/java/com/unravel/streamofthought/ui/slideshow/splashscreen.kt
@@ -3,46 +3,54 @@ package com.unravel.streamofthought.ui.slideshow
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.unravel.streamofthought.MainActivity
 import com.unravel.streamofthought.R
+import com.unravel.streamofthought.test
 
 class splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splashscreen)
         val a = 4000
-        if(FirebaseAuth.getInstance().currentUser==null){
-            Handler().postDelayed({
-                val intent: Intent = Intent(this, MainActivity ::class.java)
-                startActivity(intent)
-                finish()
-            }, a.toLong())
-        }else
-        {
-            val db = FirebaseFirestore.getInstance()
-            val mauth = FirebaseAuth.getInstance()
-            db.collection("desc").document(mauth.uid.toString()).get().addOnSuccessListener {
-                if(!it.contains("displayName"))
-                {
-                    Handler().postDelayed({
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }, a.toLong())
-                }else
-                {
-                    Handler().postDelayed({
-                        val intent: Intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }, a.toLong())
-                }
-            }
+//        if(FirebaseAuth.getInstance().currentUser==null){
+//            Handler().postDelayed({
+//                val intent: Intent = Intent(this, MainActivity ::class.java)
+//                startActivity(intent)
+//                finish()
+//            }, a.toLong())
+//        }else
+//        {
+//            val db = FirebaseFirestore.getInstance()
+//            val mauth = FirebaseAuth.getInstance()
+//            db.collection("desc").document(mauth.uid.toString()).get().addOnSuccessListener {
+//                if(!it.contains("displayName"))
+//                {
+//                    Handler().postDelayed({
+//                        val intent = Intent(this, MainActivity::class.java)
+//                        startActivity(intent)
+//                        finish()
+//                    }, a.toLong())
+//                }else
+//                {
+//                    Handler().postDelayed({
+//                        val intent: Intent = Intent(this, MainActivity::class.java)
+//                        startActivity(intent)
+//                        finish()
+//                    }, a.toLong())
+//                }
+//            }
+//
+//        }
 
-        }
+        Handler().postDelayed({
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },a.toLong())
     }
 
 }
