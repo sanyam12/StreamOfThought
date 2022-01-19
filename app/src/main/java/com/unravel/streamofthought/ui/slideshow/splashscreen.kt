fd903fdd65873dd3e7ae91a8e1a5a284d7ce8417
Akshita <akshitagoyal781@gmail.com>
2022-01-19 12:59:33 +0530

    .

---
+++ app/src/main/java/com/unravel/streamofthought/ui/slideshow/splashscreen.kt
@@ -0,0 +1,11 @@
+package com.unravel.streamofthought.ui.slideshow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
