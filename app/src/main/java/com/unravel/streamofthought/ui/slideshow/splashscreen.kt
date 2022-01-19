24b30d59a553ae9939ecc0d8d14f4cd1448d248c
Akshita <akshitagoyal781@gmail.com>
2022-01-19 13:26:02 +0530

    .

--- app/src/main/java/com/unravel/streamofthought/ui/slideshow/splashscreen.kt
+++ app/src/main/java/com/unravel/streamofthought/ui/slideshow/splashscreen.kt
@@ -1,11 +1,48 @@
 package com.unravel.streamofthought.ui.slideshow
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.unravel.streamofthought.MainActivity
+import com.unravel.streamofthought.R
 
 class splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+        val a = 4000
+        if(FirebaseAuth.getInstance().currentUser==null){
+            Handler().postDelayed({
+                val intent: Intent = Intent(this, MainActivity ::class.java)
+                startActivity(intent)
+                finish()
+            }, a.toLong())
+        }else
+        {
+            val db = FirebaseFirestore.getInstance()
+            val mauth = FirebaseAuth.getInstance()
+            db.collection("desc").document(mauth.uid.toString()).get().addOnSuccessListener {
+                if(!it.contains("displayName"))
+                {
+                    Handler().postDelayed({
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }, a.toLong())
+                }else
+                {
+                    Handler().postDelayed({
+                        val intent: Intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }, a.toLong())
+                }
+            }
+
+        }
     }
 
 }
