1cf967c35f48356a93bd02e3b0109888df77f118
Yuvraj Singh <ysrishu1031@gmail.com>
2022-01-20 20:53:52 +0530

    blog feature

--- app/src/main/java/com/unravel/streamofthought/ui/blog/BlogFragment.kt
+++ app/src/main/java/com/unravel/streamofthought/ui/blog/BlogFragment.kt
@@ -1,13 +1,19 @@
 package com.unravel.streamofthought.ui.blog
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
 import com.unravel.streamofthought.R
+import com.unravel.streamofthought.ui.blog.daos.PostDao
+import com.unravel.streamofthought.ui.blog.models.Post
 
-class BlogFragment : Fragment() {
+class BlogFragment : Fragment(), IPostAdapter {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +26,39 @@ class BlogFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener{
+            val intent = Intent(this, createPostActivity::class.java)
+            startActivity(intent)
+        }
+
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        val postDao = PostDao()
+        val postsCollections = postDao.postCollections
+        val query = postsCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+        adapter = PostAdapter(recyclerViewOptions, this)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        PostDao.updateLikes(postId)
 
     }
 }
---
+++ app/src/main/java/com/unravel/streamofthought/ui/blog/daos/UserDao.kt
@@ -0,0 +1,23 @@
+package com.unravel.streamofthought.ui.blog.daos
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.unravel.streamofthought.ui.blog.models.User
+
+class UserDao {
+    private val db = FirebaseFirestore.getInstance()
+    private val usersCollection = db.collection("users")
+
+    fun addUser(user: User?) {
+        user?.let {
+            GlobalScope.launch(Dispatchers.IO) {
+                usersCollection.document(user.uid).set(it)
+            }
+        }
+    }
+
+    fun getUserById(uId: String): Task<DocumentSnapshot> {
+        return usersCollection.document(uId).get()
+    }
+}
