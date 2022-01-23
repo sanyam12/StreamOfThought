00246ebfe35d1ee335f8bddf4f7b6df85952152b
Sanyam Ratreja <sanyamratreja18@gmail.com>
2022-01-23 21:43:19 +0530

    share button working

--- app/src/main/java/com/unravel/streamofthought/ui/home/HomeFragment.kt
+++ app/src/main/java/com/unravel/streamofthought/ui/home/HomeFragment.kt
@@ -14,8 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.unravel.streamofthought.R
 import com.unravel.streamofthought.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(), INotesRVAdapter {
-    lateinit var viewModel: noteViewModel
+class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
@@ -40,33 +39,8 @@ class HomeFragment : Fragment(), INotesRVAdapter {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView : RecyclerView = view.findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val adapter = notesRVAdapter(this,this)
-        recyclerView.adapter = adapter
-
-        viewModel = ViewModelProvider(this,
-        ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(noteViewModel::class.java)
-        viewModel.allNotes.observe(viewLifecycleOwner, Observer {list ->
-            list?.let {
-                adapter.updateList(it)
-            }
-        })
-
      }
 
-    override fun onItemClicked(notes: Notes) {
-       viewModel.deleteNote(notes)
-    }
-
-    fun submitData(view: View){
-        val input : TextView = view.findViewById(R.id.input)
-        val noteText = input.text.toString()
-        if(noteText.isNotEmpty()){
-            viewModel.insertNote(Notes(noteText))
-
-        }
-    }
 
 
     override fun onDestroyView() {
