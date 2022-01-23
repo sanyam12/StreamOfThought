f291e2edb8469d7227015b2596e8606e5d58aa88
Sanyam Ratreja <sanyamratreja18@gmail.com>
2022-01-23 17:54:07 +0530

    scroll initial

--- app/src/main/java/com/unravel/streamofthought/ui/blog/ViewBlogFragment.kt
+++ app/src/main/java/com/unravel/streamofthought/ui/blog/ViewBlogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import android.widget.TextView
 import com.unravel.streamofthought.R
 
@@ -20,14 +21,16 @@ class ViewBlogFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val scroll: ScrollView = view.findViewById(R.id.scrollView2)
+
         val text: String = requireArguments().getString("text")!!
         val tv:TextView = view.findViewById(R.id.textView9)
         tv.text = text
 
 
-        val title: String = requireArguments().getString("title")!!
-        val titleTv: TextView = view.findViewById(R.id.textView10)
-        titleTv.text = title
+//        val title: String = requireArguments().getString("title")!!
+//        val titleTv: TextView = view.findViewById(R.id.textView10)
+//        titleTv.text = title
 
     }
 
